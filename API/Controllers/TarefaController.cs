36f82ce047899a8bce6ef4ba4535931b6ab70bda
Elvertoni Coimbra <elvertoni@gmail.com>
2023-11-20 19:33:58 -0300

    first commit

---
+++ API/Controllers/TarefaController.cs
@@ -0,0 +1,90 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API.Data;
+using API.Models;
+
+namespace API.Controllers;
+
+[Route("api/tarefa")]
+[ApiController]
+public class TarefaController : ControllerBase
+{
+    private readonly AppDataContext _context;
+
+    public TarefaController(AppDataContext context) =>
+        _context = context;
+
+    // GET: api/tarefa/listar
+    [HttpGet]
+    [Route("listar")]
+    public IActionResult Listar()
+    {
+        try
+        {
+            List<Tarefa> tarefas = _context.Tarefas.Include(x => x.Categoria).ToList();
+            return Ok(tarefas);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    // POST: api/tarefa/cadastrar
+    [HttpPost]
+    [Route("cadastrar")]
+    public IActionResult Cadastrar([FromBody] Tarefa tarefa)
+    {
+        try
+        {
+            Categoria? categoria = _context.Categorias.Find(tarefa.CategoriaId);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+            tarefa.Categoria = categoria;
+            _context.Tarefas.Add(tarefa);
+            _context.SaveChanges();
+            return Created("", tarefa);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpPatch]
+    [Route("alterar/{id}")]
+    public IActionResult Alterar([FromRoute] int id,
+        [FromBody] Tarefa tarefa)
+    {
+        try
+        {
+            //Expressões lambda
+            Tarefa? tarefaCadastrada =
+                _context.Tarefas.FirstOrDefault(x => x.TarefaId == id);
+
+            if (tarefaCadastrada != null)
+            {
+
+                Categoria? categoria =
+                    _context.Categorias.Find(tarefa.CategoriaId);
+                if (categoria == null)
+                {
+                    return NotFound();
+                }
+                tarefaCadastrada.Categoria = categoria;
+                tarefaCadastrada.Titulo = tarefa.Titulo;
+                tarefaCadastrada.Descricao = tarefa.Descricao;
+                _context.Tarefas.Update(tarefaCadastrada);
+                _context.SaveChanges();
+                return Ok();
+            }
+            return NotFound();
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+}
